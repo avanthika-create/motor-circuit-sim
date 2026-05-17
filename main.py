c2fbce6949d2c9a6028b6ee3b183b34551a2c628
Avanthika <avanthikasaravanan123@gmail.com>
2026-05-16 17:22:18 -0700

    More trials, but after this commitment I need to fix the printing issue since they are all printing at the same time

--- main.py
+++ main.py
@@ -2,18 +2,35 @@ import time
 import random
 
 print("Motor Circut Simulation Lab")
+
+results = []
+
+for i in range(10): 
+
+    print("\n----------------------")
+    print(f"Trial {i+1}") # the thing that needs to be changed
+
+    # wait before stimmmm pls work 
+    time.sleep(random.uniform(1.5, 3))
+
 print("Get ready...")
 
-time.sleep(random.uniform(1,3))
+time.sleep(random.uniform(1, 2))
 
 print("GO!")
 
 start = time.time()
-
-input("Press enter as fast as possible!")
-
+input() # this has to be the user reaction
 end = time.time()
 
-reaction_time = end - start
+rt = end - start
+results.append(rt)
 
-print("Reaction Time:", round(reaction_time, 3), "seconds")
+print(f"Reaction time: {rt: .3f} seconds")
+
+# hope this pauses before the next trial
+print("Next trial starting soon...")
+time.sleep(2)
+
+print("\n EXPERIMENT COMPLETE")
+print("Average reaction time:", sum(results) / len(results))
