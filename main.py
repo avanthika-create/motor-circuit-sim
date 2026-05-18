8cec13c2b6fe359f32c3734dc7b8814aee43ca2b
Avanthika <avanthikasaravanan123@gmail.com>
2026-05-17 20:04:59 -0700

    Results are shown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 
 results = []
+mode = input("Choose mode (normal / low_dopamine / unstable):")
 
 for i in range(10): 
 
@@ -22,6 +23,14 @@ for i in range(10):
 
     reaction_time = end - start 
 
+    # dopamine part 
+    if mode == "low_dopamine":
+        reaction_time += 0.25
+
+    elif mode == "unstable":
+        import random
+        reaction_time += random.uniform(-0.15, 0.35)
+
     # saving part hopefully 
 
     results.append(reaction_time)
@@ -34,9 +43,23 @@ for i in range(10):
 
     average = sum(results) / len(results)
 
+    fastest = min(results)
+    slowest = max(results)
+
     print(f"Average Reaction Time: {average:.3f} seconds")
+    print(f"Fastest Reaction Time: {fastest:.3f} seconds")
+    print(f"Slowest Reaction Time: {slowest:.3f} seconds")
 
     print("\nAll Reaction Times:")
 
     for r in results: 
         print(f"{r:.3f}")
+
+with open("results.txt", "w") as f: 
+    f.write("Motor Circut Simulation Results\n\n")
+
+    for i in enumerate(results):
+        f.write(f"Trial {i+1}: {r: .3f} seconds\n")
+
+        avg = sum(results) / len(results)
+        f.write(f"\nAverage: {avg:.3f} seconds\n")
